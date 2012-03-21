c2c071b6491871255854cae62840a6b78eda0a54
Thomas Robitaille <thomas.robitaille@gmail.com>
2012-03-21 13:16:57 +0100

    Added version with small fonts

--- compare.py
+++ compare.py
@@ -1,3 +1,5 @@
 from matplotlib.testing.compare import compare_images
-compare_images('10.6_13.1.7/Vera.png', 'linux_13.1.7/Vera.png', 0)
-compare_images('10.6_13.1.7/cmr10.png', 'linux_13.1.7/cmr10.png', 0)
+print compare_images('10.6_13.1.7/Vera.png', 'linux_13.1.7/Vera.png', 0)
+print compare_images('10.6_13.1.7/cmr10.png', 'linux_13.1.7/cmr10.png', 0)
+print compare_images('10.6_13.1.7/Vera_small.png', 'linux_13.1.7/Vera_small.png', 0)
+print compare_images('10.6_13.1.7/cmr10_small.png', 'linux_13.1.7/cmr10_small.png', 0)
