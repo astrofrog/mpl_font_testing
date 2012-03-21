8c33616fbb4cdb67476a471b0b6efe73537e8ae7
Thomas Robitaille <thomas.robitaille@gmail.com>
2012-03-21 12:42:14 +0100

    Do tests for two different fonts

--- simpler_plot.py
+++ simpler_plot.py
@@ -6,10 +6,13 @@ mpl.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
 
-prop = fm.FontProperties(fname=os.path.abspath('Vera.ttf'), weight=0, stretch='normal', size=400)
+for font in ['Vera', 'cmr10']:
 
-fig = plt.figure(figsize=(4,4))
-fig.text(0.5, 0.5, "a", fontproperties=prop, ha='center', va='center')
-fig.savefig('hello.png', dpi=100)
-fig.savefig('hello.rgba', dpi=100)
-fig.savefig('hello.eps', dpi=100)
+    prop = fm.FontProperties(fname=os.path.abspath('%s.ttf' % font), weight=0, stretch='normal', size=400)
+
+    fig = plt.figure(figsize=(4,4))
+    fig.text(0.5, 0.5, "a", fontproperties=prop, ha='center', va='center')
+    fig.savefig('%s.png' % font, dpi=100)
+    fig.savefig('%s.rgba' % font, dpi=100)
+    fig.savefig('%s.eps' % font, dpi=100)
+
