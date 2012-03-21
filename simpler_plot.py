0b5ae947e4281059c481cb28c05e24c362c50a4a
Thomas Robitaille <thomas.robitaille@gmail.com>
2012-03-21 11:10:13 +0100

    Focus on single letter

--- simpler_plot.py
+++ simpler_plot.py
@@ -6,10 +6,10 @@ mpl.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
 
-prop = fm.FontProperties(fname=os.path.abspath('Vera.ttf'), weight=0, stretch=0, size=12)
+prop = fm.FontProperties(fname=os.path.abspath('Vera.ttf'), weight=0, stretch=0, size=400)
 
 fig = plt.figure(figsize=(4,4))
-fig.text(0.5, 0.5, "Hello, World", fontproperties=prop, ha='center', va='center')
+fig.text(0.5, 0.5, "a", fontproperties=prop, ha='center', va='center')
 fig.savefig('hello.png', dpi=100)
 fig.savefig('hello.rgba', dpi=100)
 fig.savefig('hello.eps', dpi=100)
