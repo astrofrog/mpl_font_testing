cc06f1be8e7a3f6141b64ae0c3afa072a1de0d25
Thomas Robitaille <thomas.robitaille@gmail.com>
2012-03-21 12:52:57 +0100

    Add diffs

---
+++ compare.py
@@ -0,0 +1,3 @@
+from matplotlib.testing.compare import compare_images
+compare_images('10.6_13.1.7/Vera.png', 'linux_13.1.7/Vera.png', 0)
+compare_images('10.6_13.1.7/cmr10.png', 'linux_13.1.7/cmr10.png', 0)
